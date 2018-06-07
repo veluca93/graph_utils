910e5f6256080465891ce0b74327a4bf27e7249b
Luca Versari <veluca93@gmail.com>
2018-06-07 23:41:38 +0200

    Add three triangle counting implementations

--- Makefile
+++ Makefile
@@ -1,20 +1,20 @@
-CXXFLAGS=-O3 -Wall -std=c++14 -Iutil -Igraph -flto -g
+CXXFLAGS=-O3 -Wall -std=c++14 -Iutil -Igraph -flto -g -march=native -DNDEBUG
 SDSL_CXXFLAGS=-I ~/include -L ~/lib
-LDFLAGS=-flto -lgflags -Wl,--as-needed # -ltcmalloc
+LDFLAGS=-flto -lgflags -Wl,--as-needed -march=native # -ltcmalloc
 SDSL_LDFLAGS=-lsdsl -ldivsufsort -ldivsufsort64
 CC=g++
 
 all: bin/gconvert bin/draw_graph bin/degeneracy bin/draw_info bin/compressed_adj_list \
-	bin/gpermute
+	bin/gpermute bin/compressed_triangle_counting bin/triangle_counting bin/triangle_counting_finger
 
 COMMON_OBJECTS= \
 	build/io.o build/graph.o build/nde_format.o build/el_format.o build/tsv_format.o \
-	build/oly_format.o build/bin_format.o build/graph_io.o
+	build/oly_format.o build/bin_format.o build/graph_io.o build/bel_format.o
 
 COMMON_HEADERS= \
 	graph/el_format.hpp graph/tsv_format.hpp graph/nde_format.hpp graph/graph.hpp \
 	graph/oly_format.hpp util/span.hpp util/assert.hpp util/io.hpp util/strtk.hpp \
-	graph/graph_io.hpp graph/bin_format.hpp util/common_defs.hpp
+	graph/graph_io.hpp graph/bin_format.hpp graph/bel_format.hpp util/common_defs.hpp
 
 build/io.o: util/io.cpp util/io.hpp util/strtk.hpp util/span.hpp
 	${CC} $< -o $@ -c ${CXXFLAGS}
@@ -26,6 +26,9 @@ build/nde_format.o: graph/nde_format.cpp ${COMMON_HEADERS}
 	${CC} $< -o $@ -c ${CXXFLAGS}
 
 build/el_format.o: graph/el_format.cpp ${COMMON_HEADERS}
+	${CC} $< -o $@ -c ${CXXFLAGS}
+
+build/bel_format.o: graph/bel_format.cpp ${COMMON_HEADERS}
 	${CC} $< -o $@ -c ${CXXFLAGS}
 
 build/oly_format.o: graph/oly_format.cpp ${COMMON_HEADERS}
@@ -58,6 +61,15 @@ build/draw_info.o: draw_info.cpp ${COMMON_HEADERS}
 build/compressed_adj_list.o: compressed_adj_list.cpp ${COMMON_HEADERS}
 	${CC} $< -o $@ -c ${CXXFLAGS} ${SDSL_CXXFLAGS}
 
+build/compressed_triangle_counting.o: compressed_triangle_counting.cpp ${COMMON_HEADERS}
+	${CC} $< -o $@ -c ${CXXFLAGS} ${SDSL_CXXFLAGS}
+
+build/triangle_counting.o: triangle_counting.cpp ${COMMON_HEADERS}
+	${CC} $< -o $@ -c ${CXXFLAGS}
+
+build/triangle_counting_finger.o: triangle_counting_finger.cpp ${COMMON_HEADERS}
+	${CC} $< -o $@ -c ${CXXFLAGS}
+
 bin/gconvert: build/gconvert.o ${COMMON_OBJECTS}
 	${CC} $^ -o $@ ${CXXFLAGS} ${LDFLAGS}
 
@@ -75,6 +87,15 @@ bin/gpermute: build/gpermute.o ${COMMON_OBJECTS}
 
 bin/compressed_adj_list: build/compressed_adj_list.o ${COMMON_OBJECTS}
 	${CC} $^ -o $@ ${CXXFLAGS} ${SDSL_CXXFLAGS} ${LDFLAGS} ${SDSL_LDFLAGS}
+
+bin/compressed_triangle_counting: build/compressed_triangle_counting.o ${COMMON_OBJECTS}
+	${CC} $^ -o $@ ${CXXFLAGS} ${SDSL_CXXFLAGS} ${LDFLAGS} ${SDSL_LDFLAGS}
+
+bin/triangle_counting: build/triangle_counting.o ${COMMON_OBJECTS}
+	${CC} $^ -o $@ ${CXXFLAGS} ${LDFLAGS}
+
+bin/triangle_counting_finger: build/triangle_counting_finger.o ${COMMON_OBJECTS}
+	${CC} $^ -o $@ ${CXXFLAGS} ${LDFLAGS}
 
 clean:
 	rm -f bin/* build/*
---
+++ triangle_counting_finger.cpp
@@ -0,0 +1,140 @@
+#include "common_defs.hpp"
+#include "graph_io.hpp"
+#include <immintrin.h>
+
+#ifdef __SSE4_1__
+static const int32_t cyclic_shift1_sse = _MM_SHUFFLE(0, 3, 2, 1);
+static const int32_t cyclic_shift2_sse = _MM_SHUFFLE(1, 0, 3, 2);
+static const int32_t cyclic_shift3_sse = _MM_SHUFFLE(2, 1, 0, 3);
+#endif
+
+size_t computation_size = 0;
+size_t intersection_size_smaller(span<const node_t> a, span<const node_t> b) {
+  size_t l = 0;
+  size_t res = 0;
+  for (size_t k = 0; k < a.size(); k++) {
+    computation_size++;
+    if (l >= b.size())
+      break;
+    if (a[k] < b[l]) {
+      continue;
+    }
+    if (a[k] == b[l]) {
+      res++;
+      continue;
+    }
+    size_t offset;
+    for (offset = 1; l + offset < b.size(); offset *= 2) {
+      computation_size++;
+      if (b[l + offset] >= a[k])
+        break;
+    }
+    if (l + offset >= b.size()) {
+      offset = b.size() - l;
+    }
+    size_t lower = l;
+    size_t upper = l + offset;
+    while (upper > lower + 1) {
+      computation_size++;
+      size_t middle = lower + (upper - lower) / 2;
+      if (b[middle] == a[k]) {
+        upper = middle;
+        break;
+      } else if (b[middle] < a[k]) {
+        lower = middle;
+      } else if (b[middle] > a[k]) {
+        upper = middle;
+      }
+    }
+    assert_e(a[k] > b[lower]);
+    l = upper;
+    if (l < b.size() && a[k] == b[l]) {
+      res++;
+      continue;
+    }
+  }
+  return res;
+}
+
+size_t intersection_size_same(span<const node_t> a, span<const node_t> b) {
+  size_t k = 0;
+  size_t l = 0;
+  size_t sz = 0;
+#if defined(__SSE4_1__) && NODE_T == uint32_t
+  size_t sse_end1;
+  size_t sse_end2;
+  // trim lengths to be a multiple of 4
+  sse_end1 = (a.size() / 4) * 4;
+  sse_end2 = (b.size() / 4) * 4;
+
+  while (k < sse_end1 && l < sse_end2) {
+    computation_size += 4;
+    __m128i v1_orig = _mm_loadu_si128((__m128i *)&a[k]);
+    __m128i v2_orig = _mm_loadu_si128((__m128i *)&b[l]);
+    __m128i v2 = v2_orig;
+
+    //[ move pointers
+    int32_t a_max = _mm_extract_epi32(v1_orig, 3);
+    int32_t b_max = _mm_extract_epi32(v2, 3);
+    k += (a_max <= b_max) * 4;
+    l += (a_max >= b_max) * 4;
+    //]
+
+    //[ compute mask of common elements
+    __m128i cmp_mask1_v1 = _mm_cmpeq_epi32(v1_orig, v2); // pairwise comparison
+    v2 = _mm_shuffle_epi32(v2_orig, cyclic_shift1_sse);  // shuffling
+    __m128i cmp_mask2_v1 = _mm_cmpeq_epi32(v1_orig, v2); // again...
+    v2 = _mm_shuffle_epi32(v2_orig, cyclic_shift2_sse);
+    __m128i cmp_mask3_v1 = _mm_cmpeq_epi32(v1_orig, v2); // and again...
+    v2 = _mm_shuffle_epi32(v2_orig, cyclic_shift3_sse);
+    __m128i cmp_mask4_v1 = _mm_cmpeq_epi32(v1_orig, v2); // and again.
+    __m128i cmp_mask_v1 =
+        _mm_or_si128(_mm_or_si128(cmp_mask1_v1, cmp_mask2_v1),
+                     _mm_or_si128(cmp_mask3_v1, cmp_mask4_v1));
+    int32_t mask_v1 = _mm_movemask_ps((__m128)cmp_mask_v1);
+    sz += mask_v1 ? __builtin_popcountll(mask_v1) : 0;
+  }
+#endif
+
+  while (k < a.size() && l < b.size()) {
+    computation_size++;
+    size_t av = a[k];
+    size_t bv = b[l];
+    if (av < bv) {
+      k++;
+    } else if (av > bv) {
+      l++;
+    } else {
+      sz++;
+      k++;
+      l++;
+    }
+  }
+  return sz;
+}
+
+size_t intersection_size(span<const node_t> a, span<const node_t> b) {
+  if (a.size() > b.size())
+    std::swap(a, b);
+  if (a.size() * 2 < b.size()) {
+    return intersection_size_smaller(a, b);
+  }
+  return intersection_size_same(a, b);
+}
+
+int main(int argc, char **argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  std::unique_ptr<Graph> g = ReadGraph();
+  size_t triangle_count = 0;
+  {
+    Counter cnt("Triangle counting");
+    for (uint32_t i = 0; i < g->size(); i++) {
+      for (uint32_t n : g->neighs(i)) {
+        triangle_count += intersection_size(g->neighs(i), g->neighs(n));
+        cnt++;
+      }
+    }
+  }
+  std::cerr << "Triangles:   " << triangle_count << std::endl;
+  std::cerr << "Computation: " << computation_size << std::endl;
+}
